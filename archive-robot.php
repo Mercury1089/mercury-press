af71edfd13cd17fb3069eef8632e893fe5cbcd29
Jared Tulayan <2lyNJ.E@gmail.com>
2018-05-30 23:25:28 -0400

    Structure changes
    - Remove node_modules directory
    ~ Update .gitignore
    ~ Other changes

--- archive-robot.php
+++ archive-robot.php
@@ -15,15 +15,23 @@
         while($query->have_posts()) {
             $query->the_post();
             
-            $post_img = get_post_meta('robot-icon-meta');
+            $postID = get_the_ID();
+
+            $post_img = get_post_meta($postID, 'robot-icon-meta', true);
             $robot_name = get_the_title();
+            $robot_year = get_post_meta($postID, 'robot-year-meta', true);
 
             if (empty($post_img))
                 $post_img = get_theme_file_uri('/images/default.jpg');
+
+            if (empty($robot_year)) {
+                $robot_year = '20XX';
+            }
 ?>
     <a href="<?php echo get_post_permalink(); ?>" class="robot">
         <img class="robot__icon" src="<?php echo $post_img; ?>" /> 
-        <h5 class="robot__name"><?php echo $robot_name; ?></h2>
+        <h5 class="robot__name"><?php echo $robot_name; ?></h5>
+        <h6 class="robot__year"><?php echo $robot_year; ?></h6>
     </a>
 <?php
         }
