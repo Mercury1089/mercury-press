4b5cb8a653f26fd0d7681149e8f40db5024775b6
Jared Tulayan <2lyNJ.E@gmail.com>
2018-07-28 20:57:30 -0400

    Work on hero module

--- php/header/hero.php
+++ php/header/hero.php
@@ -5,30 +5,38 @@
     $date = $post->post_date;
 
     // Create hero
-    $hero = array("hero");
+    $hero_class = array("hero");
     $content = array(
         get_the_title( $post ), 
         "By" . get_the_author_meta( 'display_name', $author_id ) . " | " . $date
     );
+    $call = null;
 
     // Hero sizes
     if (is_front_page()) // Full-height hero is for the front-page only.
-        array_push($hero, "hero--large");
+        array_push($hero_class, "hero--large");
     else if ( is_singular( array('post', 'robot') ) ) // Not-so full-page
-        array_push($hero, "hero--medium");
+        array_push($hero_class, "hero--medium");
 
     // Hero content
     if ( is_singular( 'robot' ) ) {
         $content[1] = 
             get_post_meta( $post_id, 'robot-year-meta', true ) . ": " . 
             get_post_meta( $post_id, 'robot-game-meta', true );
-    } else if ( is_front_page() ) { // Custom titling and stuff
-        $frontpage = get_post( get_option( 'page_on_front' ) );
-        $content[1] = $post->post_content;
+    } else if ( is_front_page() || is_home() ) { // Custom titling and stuff
+        if ( is_front_page() )
+            $page = get_post( get_option( 'page_on_front' ) );
+        else
+            $page = get_post( get_option( 'page_for_posts' ) );
+        
+        $content[0] = $page->post_title;
+        $content[1] = $page->post_content;
     }
 ?>
-<header class=<?php echo implode(" ", $hero); ?>>
-        <h1 class="hero__content hero__large-text"><?php echo $content[0]; ?></h1>
-        <p class="hero__content hero__small-text"><?php echo $content[1]; ?></p>
+<header class="<?php echo implode(" ", $hero_class); ?>">
+    <div class="hero__content">
+        <h1 class="hero-content__text hero-content__text--large"><?php echo $content[0]; ?></h1>
+        <p class="hero-content__text hero-content__text--small"><?php echo $content[1]; ?></p>
+    </div>
 </header>
 <main class="page-content page-content--blog">
