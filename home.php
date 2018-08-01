34c03d855a0fe8188999920c315ba0ca529fae7d
Jared Tulayan <2lyNJ.E@gmail.com>
2018-08-01 19:46:25 -0400

    Work on default layout for entire site

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-    <section class="blog-feed">
+    <section class="content__section content__section--type--feed">
     <?php 
         if (have_posts()) {
             while(have_posts()) {
--- single.php
+++ single.php
@@ -9,9 +9,9 @@
     <section class="content__section content__section--type--body">
         <?php the_content(); ?>
     </section>
-    <section class="content__section content__section--type--comments">
-        <?php comments_template(); ?>
-    </section>
+    <!-- <section class="content__section content__section--type--comments">
+        <?php // comments_template(); ?>
+    </section> -->
 <?php
             wp_reset_postdata();
         }
