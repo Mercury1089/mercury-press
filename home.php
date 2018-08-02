ec2fca9139cbe609f6e40bcbc00780c91b244e81
Jared Tulayan <2lyNJ.E@gmail.com>
2018-08-02 09:53:38 -0400

    Separate home and feed-item

--- home.php
+++ home.php
@@ -4,23 +4,10 @@
         if (have_posts()) {
             while(have_posts()) {
                 the_post(); 
-    ?>
-        <article class="blog-post">
-            <h4 class="blog-post__date"> 
-                <span class="blog-post__date-span blog-post-date__year"><?php echo get_the_date('Y'); ?></span> 
-                <span class="blog-post__date-span blog-post-date__month"><?php echo get_the_date('M'); ?></span>
-                <span class="blog-post__date-span blog-post-date__day"><?php echo get_the_date('d'); ?></span>
-            </h4>
-            <div class="blog-post__content">
-                <h4 class="blog-post__title"><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h4>
-                <h5 class="blog-post__author">Written by <?php echo get_the_author_link(); ?></h5>
-                <p class="blog-post__excerpt"><?php echo get_the_excerpt(); ?></p>
-            </div>
-        </article>
-    <?php 
+                include 'php/home/feed-item.php'; 
             } 
     ?>
-       
+    </section>
     <?php 
         } else {
             echo '<p>No content found</p>';
---
+++ php/home/feed-item.php
@@ -0,0 +1,13 @@
+<article class="feed-item">
+    <div class="feed-item__date feed-item__date--mq--large"> 
+        <h6 class="feed-item-date__line feed-item-date__line--year"><?php echo get_the_date('Y'); ?></h6> 
+        <h6 class="feed-item-date__line feed-item-date__line--month"><?php echo get_the_date('M'); ?></h6>
+        <h6 class="feed-item-date__line feed-item-date__line--day"><?php echo get_the_date('d'); ?></h6>
+    </div>
+    <div class="feed-item__body">
+        <h5 class="feed-item__title"><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h5>
+        <h6 class="feed-item__date feed-item__date--mq--small"><?php echo get_the_date( 'Y M d' ); ?></h6>
+        <h6 class="feed-item__author">Written by <?php echo get_the_author_link(); ?></h6>
+        <p class="feed-item__excerptt"><?php echo get_the_excerpt(); ?></p>
+    </div>
+</article>
