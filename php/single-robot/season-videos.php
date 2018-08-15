c58dcba554f7ac1b87098318b4976dd7b89b9b38
Jared Tulayan <2lyNJ.E@gmail.com>
2018-08-15 10:21:00 -0400

    Add default list style, add no-padding class

--- php/single-robot/season-videos.php
+++ php/single-robot/season-videos.php
@@ -5,7 +5,7 @@
     $gameReveal = get_post_meta($postID, "robot-game-reveal-meta", true);
     $robotReveal = get_post_meta($postID, "robot-robot-reveal-meta", true);
 ?>
-<ul class="grid__item grid__item--col-l--6 grid__item--col-m--4 grid__item--col-s--6 accordion-tabs">
+<ul class="no-padding grid__item grid__item--col-l--6 grid__item--col-m--4 grid__item--col-s--6 accordion-tabs">
   <li class="tab-header-and-content">
     <a href="javascript:void(0)" class="is-active tab-link">Game Reveal</a>
     <div class="tab-content">
