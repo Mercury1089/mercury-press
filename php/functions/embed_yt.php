5be14440472242497d7e581278bcdafaeafccf14
Jared Tulayan <2lyNJ.E@gmail.com>
2018-08-15 10:20:19 -0400

    Add configuration to yt embed
    Turned off related videos and enabled the white scrub bar.

--- php/functions/embed_yt.php
+++ php/functions/embed_yt.php
@@ -1,7 +1,11 @@
 <?php
     // Helper function to quickly embed YT vids
-    function embed_yt(string $id) { 
-        $src = "https://www.youtube.com/embed/{$id}"
+    function embed_yt(string $id) {
+        $opts = implode("&", array(
+            "color=white",
+            "rel=0"
+        ));
+        $src = "https://www.youtube.com/embed/{$id}?{$opts}"
 ?>
     <div class="iframe-container">
         <iframe class="iframe" src="<?php echo $src; ?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
