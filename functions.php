5debacf05063821fa47af5d9a2920eb96264bc74
Jared Tulayan <2lyNJ.E@gmail.com>
2018-08-19 20:18:07 -0400

    Format functions.php

--- functions.php
+++ functions.php
@@ -1,16 +1,15 @@
 <?php 
+    // Actions
+    require 'php/functions/actions/init.php';
+    require 'php/functions/actions/enqueue_scripts.php';
+    require 'php/functions/actions/widgets_init.php';
+    require 'php/functions/actions/after_setup_theme.php';
 
-// Actions
-require 'php/functions/actions/init.php';
-require 'php/functions/actions/enqueue_scripts.php';
-require 'php/functions/actions/widgets_init.php';
-require 'php/functions/actions/after_setup_theme.php';
-
-// Helper Functions
-require 'php/functions/helpers/bem_walker.php';
-require 'php/functions/helpers/split_content.php';
-require 'php/functions/helpers/get_title.php';
-require 'php/functions/helpers/embed_yt.php';
-require 'php/functions/helpers/generate_rand_p.php';
-require 'php/functions/helpers/get_meta_or_default.php';
+    // Helper Functions
+    require 'php/functions/helpers/bem_walker.php';
+    require 'php/functions/helpers/split_content.php';
+    require 'php/functions/helpers/get_title.php';
+    require 'php/functions/helpers/embed_yt.php';
+    require 'php/functions/helpers/generate_rand_p.php';
+    require 'php/functions/helpers/get_meta_or_default.php';
 ?>
