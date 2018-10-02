bdc89a9ceeb7054c69c9971a1194a45538d129db
Jared Tulayan <2lyNJ.E@gmail.com>
2018-10-02 15:37:45 -0400

    Change prev and next pagination text in blog feed

--- home.php
+++ home.php
@@ -23,8 +23,8 @@
     echo paginate_links( array(
         'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
         'format' => '?paged=%#%',
-        'prev_text'          => __('« Prev'),
-	    'next_text'          => __('Next »'),
+        'prev_text'          => __('«'),
+	    'next_text'          => __('»'),
         'current' => max( 1, get_query_var('paged') ),
         'total' => $wp_query->max_num_pages
     ) );
