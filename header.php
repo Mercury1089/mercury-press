a95c18c837b7605785f1e95705ba34f601cf4e14
Jared Tulayan <2lyNJ.E@gmail.com>
2019-03-25 13:02:02 -0400

    Edit meta to properly display in Discord

--- header.php
+++ header.php
@@ -4,6 +4,11 @@
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <title><?php echo get_title(); ?></title>
+        <meta property="og:url" content="http://mercury1089.com" />
+        <meta property="og:title" content="Team Mercury 1089" />
+        <meta property="og:description" content="Hightstown FRC Team 1089" />
+        <meta property="og:image" content="http://mercury1089.com/wp-content/uploads/images/thumb.png" />
+        <meta property="og:type" content="website" />
         <?php wp_head(); ?>
     </head>
     
