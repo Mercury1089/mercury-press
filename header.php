fe39d5f3b7f086a3f5d6a0ca1f7a012905a575ed
Jared Tulayan <2lyNJ.E@gmail.com>
2019-03-25 19:25:22 -0400

    Add open graph meta for unfurling

--- header.php
+++ header.php
@@ -3,12 +3,15 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-        <title><?php echo get_title(); ?></title>
+        <meta property="og:type" content="website" />
         <meta property="og:url" content="http://mercury1089.com" />
         <meta property="og:title" content="Team Mercury 1089" />
+        <meta property="og:site_title" content="Team Mercury" />
+        <meta property="og:article" content="" />
+        <meta property="og:profile" content="" />
         <meta property="og:description" content="Hightstown FRC Team 1089" />
         <meta property="og:image" content="http://mercury1089.com/wp-content/uploads/images/thumb.png" />
-        <meta property="og:type" content="website" />
+        <title><?php echo get_title(); ?></title>
         <?php wp_head(); ?>
     </head>
     
